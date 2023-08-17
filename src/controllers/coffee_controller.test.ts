9ef6f7588d6ceb26cfd6b474674a617df58c05bd
Camh-git <cameronhillier1@gmail.com>
2023-08-17 21:37:27 +0100

    step 9
    Added extra tests
    fixed package.json having "test" be the start and "start" being the default.

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -8,3 +8,21 @@ test("GET /coffee should return correct object", async () => {
     name: "Latte",
   });
 });
+test("/coffee get without param should return default", async () => {
+  const res = await request(app).get("/coffee");
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
+test("/coffee get with non-default param should return correct object", async () => {
+  const res = await request(app)
+    .get("/coffee")
+    .query({ coffeeName: "espresso" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "espresso",
+  });
+});
