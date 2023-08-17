d3b41f7f2cfd61e92e812c736a205b0db103daf4
Camh-git <cameronhillier1@gmail.com>
2023-08-17 17:27:27 +0100

    Added coffee lovers endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
 import express from "express";
 export const app = express();
 app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
+app.get("/coffelover", (req, res) => res.send("I like coffee!"));
