fee034b11d34b5b2d59889822a6a148fba43a0fe
Camh-git <cameronhillier1@gmail.com>
2023-08-17 22:15:24 +0100

    Fixed typos in coffe_lover tests
    apparently I misspelled the coffeelover endpoint as coffelover the first time, so when we moved it the tests broke.

--- src/controllers/coffee_lover.test.ts
+++ src/controllers/coffee_lover.test.ts
@@ -2,8 +2,8 @@ import request from "supertest";
 import { app } from "../app";
 describe("Test coffee lover API endpoint request", () => {
   test("GET should return correct message", async () => {
-    const res = await request(app).get("/coffelover");
+    const res = await request(app).get("/coffeelover");
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toEqual("I love coffee!");
+    expect(res.text).toEqual('"I love coffee"');
   });
 });
